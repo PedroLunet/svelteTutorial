e95e250f721de453287c4f0273bb68b9a53dd6ed
Pedro Lunet <pedrolunet@significa.co>
2025-07-10 15:10:39 +0100

    CSS & Conditional styles

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,14 +1,38 @@
 <script>
 	let showModal = true;
+	let isPromo = true;
 </script>
 
 {#if showModal}
-	<div class="backdrop">
+	<div class="backdrop" class:promo={isPromo}>
 		<div class="modal">
-			<p>This is a modal!</p>
+			<p>@Significa</p>
 		</div>
 	</div>
 {/if}
 
 <style>
+	.backdrop {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.55);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.modal {
+		background-color: white;
+		padding: 2rem;
+		border-radius: 14px;
+		box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+	}
+
+	.promo {
+		background-color: crimson;
+		color: orangered;
+	}
 </style>
