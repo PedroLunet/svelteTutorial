e04b3724ceff37bf18812887d59e015637dd46b2
Pedro Lunet <pedrolunet@significa.co>
2025-07-11 11:14:41 +0100

    Header and Footer components

---
+++ sveltePools/src/components/Header.svelte
@@ -0,0 +1,20 @@
+<header>
+	<h1>
+		<img src="/img/logo.svg" alt="Pool Tutorial Logo" />
+	</h1>
+</header>
+
+<style>
+	header {
+		background-color: #f7f7f7;
+		padding: 20px;
+	}
+
+	h1 {
+		margin: 0;
+		text-align: center;
+	}
+	img {
+		width: 120px;
+	}
+</style>
