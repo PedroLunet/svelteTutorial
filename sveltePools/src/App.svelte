404c8272d2fe2c0be3266c5a305eacce323265d9
Pedro Lunet <pedrolunet@significa.co>
2025-07-11 11:49:24 +0100

    Reusable Tabs Component

--- sveltePools/src/App.svelte
+++ sveltePools/src/App.svelte
@@ -1,10 +1,27 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import Tabs from './shared/Tabs.svelte';
+
+	//tabs
+	let items = ['Current Pools', 'Add New Pool'];
+	let activeItem = 'Current Pools';
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	};
 </script>
 
 <Header />
-<main></main>
+<main>
+	<Tabs {items} {activeItem} on:tabChange={tabChange} />
+	{#if activeItem === 'Current Pools'}
+		<!-- Current Pools content goes here -->
+		<p>Displaying current pools...</p>
+	{:else if activeItem === 'Add New Pool'}
+		<!-- Add New Pool content goes here -->
+		<p>Form to add a new pool...</p>
+	{/if}
+</main>
 <Footer />
 
 <style>
---
+++ sveltePools/src/shared/Tabs.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	export let items = [];
+	export let activeItem = '';
+</script>
+
+<div class="tabs">
+	<ul>
+		{#each items as item}
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<li on:click={() => dispatch('tabChange', item)}>
+				<div class:active={item === activeItem}>{item}</div>
+			</li>
+		{/each}
+	</ul>
+</div>
+
+<style>
+	.tabs {
+		margin-bottom: 40px;
+	}
+	ul {
+		list-style-type: none;
+		padding: 0;
+		display: flex;
+		justify-content: center;
+	}
+
+	li {
+		margin: 0 16px;
+		font-size: 18px;
+		color: #555;
+		cursor: pointer;
+	}
+
+	.active {
+		color: orangered;
+		border-bottom: 2px solid orangered;
+		padding-bottom: 8px;
+	}
+</style>
