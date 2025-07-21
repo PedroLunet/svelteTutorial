9991a3ca84a0c8ed483431fcf3669c091d0d04fa
Pedro Lunet <pedrolunet@significa.co>
2025-07-21 12:19:33 +0100

    Card Component

--- sveltePools/src/components/PoolDetails.svelte
+++ sveltePools/src/components/PoolDetails.svelte
@@ -1,22 +1,35 @@
 <script>
+	import Card from '../shared/Card.svelte';
 	export let pool;
 
 	//Reactive values
 	$: totalVotes = pool.votesA + pool.votesB;
 </script>
 
-<div class="Poll">
-	<h3>{pool.question}</h3>
-	<p>Total Votes: {totalVotes}</p>
-	<div class="answer">
-		<div class="percent percent-a"></div>
-		<span> {pool.answerA} ({pool.votesA})</span>
+<Card>
+	<div class="Poll">
+		<h3>{pool.question}</h3>
+		<p>Total Votes: {totalVotes}</p>
+		<div class="answer">
+			<div
+				class="percent percent-a"
+				style="width: {totalVotes
+					? (pool.votesA / totalVotes) * 100 + '%'
+					: '0%'}"
+			></div>
+			<span> {pool.answerA} ({pool.votesA})</span>
+		</div>
+		<div class="answer">
+			<div
+				class="percent percent-b"
+				style="width: {totalVotes
+					? (pool.votesB / totalVotes) * 100 + '%'
+					: '0%'}"
+			></div>
+			<span> {pool.answerB} ({pool.votesB})</span>
+		</div>
 	</div>
-	<div class="answer">
-		<div class="percent percent-b"></div>
-		<span> {pool.answerB} ({pool.votesB})</span>
-	</div>
-</div>
+</Card>
 
 <style>
 	h3 {
